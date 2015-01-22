3272f3a2a63d66dd87e4733a705705b948c8e197
Milad Barsomo <Barsomisco@gmail.com>
2015-01-22 12:40:37 +0100

    fixed input validation

--- lab1/client.js
+++ lab1/client.js
@@ -1,8 +1,23 @@
 displayView = function(){
     // the code required to display a view
-
+    document.getElementById("show welcomeview").innerHTML = document.getElementById("welcomeview").innerHTML;
 };
 
 window.onload = function(){
-    document.getElementById("show welcomeview").innerHTML = document.getElementById("welcomeview").innerHTML;
+    displayView();
 };
+
+signUpValidation = function(){
+    var password = document.forms["signUpForm"]["signUpPass"].value;
+    if (password.length < 7){
+        alert("Password should be at least 7 characters long");
+        return false;
+    }
+    var repeatedPassword = document.forms["signUpForm"]["repeatPass"].value;
+    if (repeatedPassword != password) {
+        alert("Passwords do not match");
+        return false;
+    }
+};
+
+
