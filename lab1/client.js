8bacd093aa64ee76250223a0be3a9a1a305ff31b
samuelnilsson <samni698@gmail.com>
2015-01-22 15:56:09 +0100

    Changed the property access to dor notation instead of bracket notation.

--- lab1/client.js
+++ lab1/client.js
@@ -1,6 +1,6 @@
 displayView = function(){
     // the code required to display a view
-    document.getElementById("show welcomeview").innerHTML = document.getElementById("welcomeview").innerHTML;
+    document.getElementById("showwelcomeview").innerHTML = document.getElementById("welcomeview").innerHTML;
 };
 
 window.onload = function(){
@@ -8,7 +8,7 @@ window.onload = function(){
 };
 
 logInValidation = function(){
-    var password = document.forms["logInForm"]["logInPass"].value;
+    var password = document.forms.logInForm.logInPass.value;
     if (password.length < 7){
         alert("Password should be at least 7 characters long");
         return false;
@@ -16,12 +16,12 @@ logInValidation = function(){
 };
 
 signUpValidation = function(){
-    var password = document.forms["signUpForm"]["signUpPass"].value;
+    var password = document.forms.signUpForm.signUpPass.value;
     if (password.length < 7){
         alert("Password should be at least 7 characters long");
         return false;
     }
-    var repeatedPassword = document.forms["signUpForm"]["repeatPass"].value;
+    var repeatedPassword = document.forms.repeatPass.value;
     if (repeatedPassword != password) {
         alert("Passwords do not match");
         return false;
