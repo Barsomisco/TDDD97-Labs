0934532c1e09cab9ba9015c861903a7d19eee5e8
Milad Barsomo <Barsomisco@gmail.com>
2015-01-22 12:54:02 +0100

    fixed login validation

--- lab1/client.js
+++ lab1/client.js
@@ -7,6 +7,14 @@ window.onload = function(){
     displayView();
 };
 
+logInValidation = function(){
+    var password = document.forms["logInForm"]["logInPass"].value;
+    if (password.length < 7){
+        alert("Password should be at least 7 characters long");
+        return false;
+    }
+};
+
 signUpValidation = function(){
     var password = document.forms["signUpForm"]["signUpPass"].value;
     if (password.length < 7){
