a6e1ed4aab7a81b1372873e7a8cda8757103e190
samuelnilsson <samni698@gmail.com>
2015-02-09 15:43:00 +0100

    Added sign out and get user data functionality. The token are also stored in the database.

--- lab2/database_helper.py
+++ lab2/database_helper.py
@@ -1,14 +1,17 @@
 import sqlite3
 from flask import g
 
+
 def connect_db():
     return sqlite3.connect('database.db')
+
 
 def get_db():
     db = getattr(g, 'db', None)
     if db is None:
         db = g.db = connect_db()
     return db
+
 
 def add_user(email, password, firstname, familyname, gender, city, country):
     c = connect_db()
@@ -20,6 +23,7 @@ def add_user(email, password, firstname, familyname, gender, city, country):
         return False
     return True
 
+
 def get_password(email):
     c = connect_db()
     cur = c.cursor()
@@ -30,6 +34,43 @@ def get_password(email):
         return result[0]
     except:
         return False
+
+
+def add_token(email, token):
+    c = connect_db()
+    user = (token, email)
+    try:
+        c.execute('''INSERT INTO logged_in_users VALUES(?, ?)''', user)
+        c.commit()
+        return True
+    except:
+        return False
+
+
+def remove_token(token):
+    c = connect_db()
+    tok = (token,)
+    try:
+        c.execute('''DELETE FROM logged_in_users WHERE token=?''', tok)
+        c.commit()
+        return True
+    except:
+        return False
+
+
+def get_user_data(token):
+    c = connect_db()
+    cur = c.cursor()
+    tok = (token,)
+    try:
+        cur.execute('''SELECT email FROM logged_in_users WHERE token=?''', tok)
+        email = cur.fetchone()
+        cur.execute('''SELECT * FROM users WHERE email=?''', email)
+        result = cur.fetchone()
+        return [result[0], result[2], result[3], result[4], result[5], result[6]]
+    except:
+        return False
+
 
 def close():
     get_db().close()
