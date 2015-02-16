ebe3e285d4621e6badaca17b63649b624dded475
samuelnilsson <samni698@gmail.com>
2015-02-16 10:19:59 +0100

    The signup and signout functionality is now requesting the server instead of serverstub.

--- Twidder/Twidder/static/client.js
+++ Twidder/Twidder/static/client.js
@@ -40,7 +40,8 @@ logInValidation = function(signInForm) {
 checkRequiredPassword = function() {
     var password = document.getElementById("password").value;
     var repeatedPassword = document.getElementById("repeatpassword").value;
-    var message = document.getElementById("message");
+    var message = document.getElementById("signupmessage");
+    message.style.color = "Red";
     if (password.length < 7) {
         message.innerHTML = "Password is to short";
         return false;
@@ -93,165 +94,190 @@ postText = function() {
         //       updateMessages(recipient);
         //       document.getElementsByName("posttextarea")[0].value = "";
         //       return result.success;
-        //   }
-    };
+    }
+};
 
-    checkPassword = function() {
-        var password = document.getElementById("password").value;
-        var message = document.getElementById("message");
-        if (password.length < 7) {
-            message.innerHTML = "Password is to short";
-            return false;
-        } else if (!checkRequiredPassword()) {
-            return false;
-        } else {
-            message.innerHTML = "";
-            return false;
-        }
-    };
-
-    checkNewPassword = function(newpassword) {
-        var password = newpassword.value;
-        var message = document.getElementById("newpassmessage");
-        if (password.length < 7) {
-            message.innerHTML = "Password is to short";
-            return false;
-        }
+checkPassword = function() {
+    var password = document.getElementById("password").value;
+    var message = document.getElementById("signupmessage");
+    message.style.color = "Red";
+    if (password.length < 7) {
+        message.innerHTML = "Password is to short";
+        return false;
+    } else if (!checkRequiredPassword()) {
+        return false;
+    } else {
         message.innerHTML = "";
-        return true;
+        return false;
+    }
+};
+
+checkNewPassword = function(newpassword) {
+    var password = newpassword.value;
+    var message = document.getElementById("newpassmessage");
+    if (password.length < 7) {
+        message.innerHTML = "Password is to short";
+        return false;
+    }
+    message.innerHTML = "";
+    return true;
+};
+
+changePassword = function(formData) {
+    var token = localStorage.getItem("token");
+    var oldPassword = formData.oldpassword.value;
+    var newPassword = formData.newpassword.value;
+    var message = document.getElementById("newpassmessage");
+    if (newPassword.length < 7) {
+        message.innerHTML = "Password is to short";
+        return false;
+    }
+    var result = serverstub.changePassword(token, oldPassword, newPassword);
+    message.innerHTML = result.message;
+    return result.success;
+};
+
+signUpValidation = function(formData) {
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 & xmlhttp.status == 200) {
+            data = JSON.parse(xmlhttp.responseText);
+            console.log(data.success);
+            if (data.success) {
+                document.getElementById("signupmessage").style.color = "Green";
+                document.getElementById("signupmessage").innerHTML = data.message;
+            }
+            else {
+                document.getElementById("signupmessage").innerHTML = data.message;
+            }
+        }
     };
 
-    changePassword = function(formData) {
-        var token = localStorage.getItem("token");
-        var oldPassword = formData.oldpassword.value;
-        var newPassword = formData.newpassword.value;
-        var message = document.getElementById("newpassmessage");
-        if (newPassword.length < 7) {
-            message.innerHTML = "Password is to short";
-            return false;
-        }
-        var result = serverstub.changePassword(token, oldPassword, newPassword);
-        message.innerHTML = result.message;
-        return result.success;
+    var message = document.getElementById("signupmessage");
+    message.style.color = "Red";
+    var password = formData.password.value;
+    if (password.length < 7) {
+        message.innerHTML = "Password is too short";
+        return false;
+    }
+    var repeatedPassword = formData.repeatpassword.value;
+    if (repeatedPassword != password) {
+        message.innerHTML = "Passwords do not match";
+        return false;
+    }
+
+    var regData = {
+        "firstname": formData.firstname.value,
+        "familyname": formData.familyname.value,
+        "gender": formData.gender.value,
+        "city": formData.city.value,
+        "country": formData.country.value,
+        "email": formData.email.value,
+        "password": formData.password.value
     };
 
-    signUpValidation = function(formData) {
-        var password = formData.password.value;
-        if (password.length < 7) {
-            return false;
-        }
-        var repeatedPassword = formData.repeatpassword.value;
-        if (repeatedPassword != password) {
-            return false;
-        }
+    xmlhttp.open("POST", "/signup", true);
+    var params = "email="+regData.email+"&password="+regData.password+"&firstname="+regData.firstname+"&familyname="+regData.familyname+"&gender="+regData.gender+"&city="+regData.city+"&country="+regData.country;
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(params);
 
-        var regData = {
-            "firstname": formData.firstname.value,
-            "familyname": formData.familyname.value,
-            "gender": formData.gender.value,
-            "city": formData.city.value,
-            "country": formData.country.value,
-            "email": formData.email.value,
-            "password": formData.password.value
-        };
+};
 
-        var message = document.getElementById("message");
-        var m = serverstub.signUp(regData);
-        message.innerHTML = m.message;
-        if (m.success) {
-            var result = serverstub.signIn(formData.email.value, formData.password.value);
-            localStorage.token = result.data;
-            document.getElementById("view").innerHTML = document.getElementById("profileview").innerHTML;
-            selected(document.getElementById("home"));
-            return true;
+signOut = function() {
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 & xmlhttp.status == 200) {
+            data = JSON.parse(xmlhttp.responseText);
+            console.log(data.success);
+            if (data.success) {
+                document.getElementById("view").innerHTML = document.getElementById("welcomeview").innerHTML;
+            }
+
+        }
+    };
+    var token = localStorage.getItem("token");
+    localStorage.removeItem("token");
+    xmlhttp.open("POST", "/signout", true);
+    var params = "token="+token;
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(params);
+};
+
+selected = function(item) {
+    item.style.backgroundColor = "#999999";
+
+    for (var i = 0; i < item.parentNode.childNodes.length; ++i) {
+        if (item.parentNode.childNodes[i].nodeType == Node.ELEMENT_NODE && item.parentNode.childNodes[i].innerHTML != item.innerHTML)
+            item.parentNode.childNodes[i].style.backgroundColor = "gray";
+    }
+    tab = item.innerHTML;
+
+    if (item.innerHTML == "Home") {
+        document.getElementById("homeview").style.display = "block";
+        document.getElementById("browseview").style.display = "none";
+        document.getElementById("accountview").style.display = "none";
+        updateHome();
+        updateMessages();
+    } else if (item.innerHTML == "Browse") {
+        document.getElementById("homeview").style.display = "none";
+        document.getElementById("browseview").style.display = "block";
+        document.getElementById("accountview").style.display = "none";
+        searchUser();
+    } else {
+        document.getElementById("homeview").style.display = "none";
+        document.getElementById("browseview").style.display = "none";
+        document.getElementById("accountview").style.display = "block";
+    }
+};
+
+updateHome = function(email) {
+    if (email == null) {
+        email = serverstub.getUserDataByToken(localStorage.getItem("token")).data.email;
+    }
+
+    var userdata = serverstub.getUserDataByEmail(localStorage.getItem("token"), email).data;
+    document.getElementById("loggedinname").innerHTML = userdata.firstname;
+    document.getElementById("loggedinfamilyname").innerHTML = userdata.familyname;
+    document.getElementById("loggedingender").innerHTML = userdata.gender;
+    document.getElementById("loggedincity").innerHTML = userdata.city;
+    document.getElementById("loggedincountry").innerHTML = userdata.country;
+    document.getElementById("loggedinemail").innerHTML = userdata.email;
+};
+
+updateMessages = function(email) {
+    if (email == null) {
+        email = serverstub.getUserDataByToken(localStorage.getItem("token")).data.email;
+    }
+    var messages = serverstub.getUserMessagesByEmail(localStorage.getItem("token"), email).data;
+    for (var i = 0; i<messages.length; ++i) {
+        if (i === 0)
+            document.getElementById("messages").innerHTML = "<div>" + messages[i].writer + " - " + messages[i].content + "</div>";
+        else
+            document.getElementById("messages").innerHTML += "<div>"+ messages[i].writer + " - " + messages[i].content + "</div>";
+    }
+    return messages.success;
+};
+
+searchUser = function(formData) {
+    var email;
+    if (formData == null) {
+        email = lastsearched;
+
+    }
+    else {
+        email = formData.searchemail.value;
+        lastsearched = email;
+    }
+    var result = serverstub.getUserDataByEmail(localStorage.getItem("token"), email);
+    if (result.success) {
+        updateHome(email);
+        updateMessages(email);
+        document.getElementById("userpage").innerHTML = document.getElementById("homeview").innerHTML;
+        document.getElementsByName("header")[1].innerHTML = "";
+        document.getElementById("wrongemail").innerHTML = "";
+    }
+    else {
+        if (formData != null) {
+            document.getElementById("wrongemail").innerHTML = result.message;
         }
         return false;
-    };
-
-    signOut = function() {
-        var token = localStorage.getItem("token");
-        localStorage.removeItem("token");
-        var result = serverstub.signOut(token);
-        document.getElementById("view").innerHTML = document.getElementById("welcomeview").innerHTML;
-    };
-
-    selected = function(item) {
-        item.style.backgroundColor = "#999999";
-
-        for (var i = 0; i < item.parentNode.childNodes.length; ++i) {
-            if (item.parentNode.childNodes[i].nodeType == Node.ELEMENT_NODE && item.parentNode.childNodes[i].innerHTML != item.innerHTML)
-                item.parentNode.childNodes[i].style.backgroundColor = "gray";
-        }
-        tab = item.innerHTML;
-
-        if (item.innerHTML == "Home") {
-            document.getElementById("homeview").style.display = "block";
-            document.getElementById("browseview").style.display = "none";
-            document.getElementById("accountview").style.display = "none";
-            updateHome();
-            updateMessages();
-        } else if (item.innerHTML == "Browse") {
-            document.getElementById("homeview").style.display = "none";
-            document.getElementById("browseview").style.display = "block";
-            document.getElementById("accountview").style.display = "none";
-            searchUser();
-        } else {
-            document.getElementById("homeview").style.display = "none";
-            document.getElementById("browseview").style.display = "none";
-            document.getElementById("accountview").style.display = "block";
-        }
-    };
-
-    updateHome = function(email) {
-        if (email == null) {
-            email = serverstub.getUserDataByToken(localStorage.getItem("token")).data.email;
-        }
-
-        var userdata = serverstub.getUserDataByEmail(localStorage.getItem("token"), email).data;
-        document.getElementById("loggedinname").innerHTML = userdata.firstname;
-        document.getElementById("loggedinfamilyname").innerHTML = userdata.familyname;
-        document.getElementById("loggedingender").innerHTML = userdata.gender;
-        document.getElementById("loggedincity").innerHTML = userdata.city;
-        document.getElementById("loggedincountry").innerHTML = userdata.country;
-        document.getElementById("loggedinemail").innerHTML = userdata.email;
-    };
-
-    updateMessages = function(email) {
-        if (email == null) {
-            email = serverstub.getUserDataByToken(localStorage.getItem("token")).data.email;
-        }
-        var messages = serverstub.getUserMessagesByEmail(localStorage.getItem("token"), email).data;
-        for (var i = 0; i<messages.length; ++i) {
-            if (i === 0)
-                document.getElementById("messages").innerHTML = "<div>" + messages[i].writer + " - " + messages[i].content + "</div>";
-            else
-                document.getElementById("messages").innerHTML += "<div>"+ messages[i].writer + " - " + messages[i].content + "</div>";
-        }
-        return messages.success;
-    };
-
-    searchUser = function(formData) {
-        var email;
-        if (formData == null) {
-            email = lastsearched;
-
-        }
-        else {
-            email = formData.searchemail.value;
-            lastsearched = email;
-        }
-        var result = serverstub.getUserDataByEmail(localStorage.getItem("token"), email);
-        if (result.success) {
-            updateHome(email);
-            updateMessages(email);
-            document.getElementById("userpage").innerHTML = document.getElementById("homeview").innerHTML;
-            document.getElementsByName("header")[1].innerHTML = "";
-            document.getElementById("wrongemail").innerHTML = "";
-        }
-        else {
-            if (formData != null) {
-                document.getElementById("wrongemail").innerHTML = result.message;
-            }
-            return false;
-        }
-    };
+    }
+};
